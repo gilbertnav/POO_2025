4e7398800b031d652f45f7ca41618ba131cb7c36
gilbertnav <gilbertnav@outlook.com>
2025-03-26 19:10:20 -0600

    ok

--- E16PedirDatos/src/main/java/poo/e16pedirdatos/E16PedirDatos.java
+++ E16PedirDatos/src/main/java/poo/e16pedirdatos/E16PedirDatos.java
@@ -20,17 +20,14 @@ public class E16PedirDatos {
         estatura = new float[10];
         //Pedir nombres
         for (int i = 0; i < 10; i++) {
-            nombre[i]=JOptionPane.showInputDialog(null, "Nombre" + (i+1), "Recopilar datos", 0, iconoUsuario, null, null).toString();
-           // nombre[i] = JOptionPane.showInputDialog("Nombre " + (i+1));
+           nombre[i]=JOptionPane.showInputDialog(null, "Nombre" + (i+1), "Recopilar datos", 0, iconoUsuario, null, null).toString();
            estatura[i] = Float.parseFloat(JOptionPane.showInputDialog(null, "Estatura de " + nombre[i], "Recopilar datos", 0, iconoEstatura, null, null).toString());
-           // estatura[i] = Float.parseFloat(JOptionPane.showInputDialog("Estatura de " + nombre[i]));
         }
         //Visualizar los nombres
         StringBuilder mensaje = new StringBuilder();
         for (int i = 0; i < 10; i++) {
             mensaje.append(nombre[i]).append("      ").append(estatura[i]).append("\n");
         }
-        //JOptionPane.showMessageDialog(null, mensaje);
         JOptionPane.showMessageDialog(null, mensaje, "Reporte", 0, iconoReporte);
     }
 }
