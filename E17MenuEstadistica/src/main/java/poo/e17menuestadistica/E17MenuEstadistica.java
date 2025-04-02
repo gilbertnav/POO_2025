b5f04155360b37a93ea2aecda270a41f3a41ebcd
gilbertnav <gilbertnav@outlook.com>
2025-04-02 05:05:06 -0600

    2 abril 2025

--- E17MenuEstadistica/src/main/java/poo/e17menuestadistica/E17MenuEstadistica.java
+++ E17MenuEstadistica/src/main/java/poo/e17menuestadistica/E17MenuEstadistica.java
@@ -6,6 +6,7 @@ import javax.swing.JPasswordField;
 
 public class E17MenuEstadistica {
 
+    private static String usuario;
     private static String[] nombre;
     private static float[] estatura;
     private static ImageIcon iconoUsuario, iconoPassword, iconoError, iconoSesionDenegada;
@@ -13,13 +14,59 @@ public class E17MenuEstadistica {
 
     public static void main(String[] args) {
         inicializarSistema();
+        boolean esUsuarioValido = validarUsuario();
+        //Si el usuario es válido damos la bienvenida y mostramos el menú
+        if (esUsuarioValido) {
+            JOptionPane.showMessageDialog(null, "Bienvenido al sistema: " + usuario, "Sistema estadísico", 0, iconoUsuario);
+            String menu = """
+                             Menu de opciones
+                             
+                             [1] Registrar
+                             [2] Reporte
+                             [3] Salir
+                             
+                             Elige una opción
+                             """;
+            int opcion;
+            do {
+                opcion = Integer.parseInt(JOptionPane.showInputDialog(null, menu, "Sesión activa: " + usuario, 0, iconoMenu, null, null).toString());
+                switch (opcion) {
+                    case 1:registrarDatos();
+                        break;
+                    case 2:mostrarReporte();
+                        break;
+                    case 3:
+                        break;
+                    default:
+                        JOptionPane.showMessageDialog(null, "Opcion no válida", "Error", 0, iconoError);
+                }
+            } while (opcion != 3);
+        } else {
+            JOptionPane.showMessageDialog(null, "Intentos agotados", "Sesión denegada", 0, iconoSesionDenegada);
+        }
+    }
+
+    //Metod que no devuelve valor y no necesita paràmetro
+    private static void inicializarSistema() {
+        String path = "src/main/java/imagenes/";
+        iconoUsuario = new ImageIcon(path + "usuario.png");
+        iconoPassword = new ImageIcon(path + "password.png");
+        iconoError = new ImageIcon(path + "error.png");
+        iconoSesionDenegada = new ImageIcon(path + "denegado.png");
+        iconoRegistrar = new ImageIcon(path + "registrar.png");
+        iconoEstatura = new ImageIcon(path + "estatura.png");
+        iconoReporte = new ImageIcon(path + "reporte.png");
+        iconoMenu = new ImageIcon(path + "menuPrincipal.png");
+        nombre = new String[10];
+        estatura = new float[10];
+    }
+
+    private static boolean validarUsuario() {
         //Constantes validas para el inicion de sesión
         final String USUARIO_VALIDO = "Gilberto";
         final String PASSWORD_VALIDO = "789";
         //Declaracion de las variables
-        String usuario;
         String password;
-
         //Objeto para enmascarar la contraseña
         JPasswordField passwordOculto = new JPasswordField();
         //Pedir datos de entrada
@@ -41,60 +88,24 @@ public class E17MenuEstadistica {
                 JOptionPane.showMessageDialog(null, "Usuario o password incorrecto", "Error", 0, iconoError);
             }
         } while (!esUsuarioValido && intentosFallidos < 3);
-        //Si el usuario es válido damos la bienvenida y mostramos el menú
-        if (esUsuarioValido) {
-            JOptionPane.showMessageDialog(null, "Bienvenido al sistema: " + usuario, "Sistema estadísico", 0, iconoUsuario);
-            String menu = """
-                             Menu de opciones
-                             
-                             [1] Registrar
-                             [2] Reporte
-                             [3] Salir
-                             
-                             Elige una opción
-                             """;
-            int opcion;
-            do {
-                opcion = Integer.parseInt(JOptionPane.showInputDialog(null, menu, "Sesión activa: " + usuario, 0, iconoMenu, null, null).toString());
-                switch (opcion) {
-                    case 1:
-                        for (int i = 0; i < 10; i++) {
-                            nombre[i] = JOptionPane.showInputDialog(null, "Nombre ", "Registro " + (i + 1), 0, iconoUsuario, null, null).toString();
-                            estatura[i] = Float.parseFloat(JOptionPane.showInputDialog(null, "Estatura de " + nombre[i], "Registro " + (i + 1), 0, iconoEstatura, null, null).toString());
-                        }
-                        break;
-                    case 2:
-                        StringBuilder mensaje = new StringBuilder();
-                        for (int i = 0; i < 10; i++) {
-                            mensaje.append(nombre[i]).append("      ").append(estatura[i]).append("\n");
-                        }
-                        JOptionPane.showMessageDialog(null, mensaje, "Reporte", 0, iconoReporte);
-                        break;
-                    case 3:
-                        break;
-                    default:
-                        JOptionPane.showMessageDialog(null, "Opcion no válida", "Error", 0, iconoError);
-                }
-            } while (opcion != 3);
+        return esUsuarioValido;
+    }
 
-        } else {
-            JOptionPane.showMessageDialog(null, "Intentos agotados", "Sesión denegada", 0, iconoSesionDenegada);
+    private static void registrarDatos() {
+        for (int i = 0; i < 10; i++) {
+            nombre[i] = JOptionPane.showInputDialog(null, "Nombre ", "Registro " + (i + 1), 0, iconoUsuario, null, null).toString();
+            estatura[i] = Float.parseFloat(JOptionPane.showInputDialog(null, "Estatura de " + nombre[i], "Registro " + (i + 1), 0, iconoEstatura, null, null).toString());
         }
     }
 
-    //Metod que no devuelve valor y no necesita paràmetro
-    private static void inicializarSistema() {
-        String path = "src/main/java/imagenes/";
-        iconoUsuario = new ImageIcon(path + "usuario.png");
-        iconoPassword = new ImageIcon(path + "password.png");
-        iconoError = new ImageIcon(path + "error.png");
-        iconoSesionDenegada = new ImageIcon(path + "denegado.png");
-        iconoRegistrar = new ImageIcon(path + "registrar.png");
-        iconoEstatura = new ImageIcon(path + "estatura.png");
-        iconoReporte = new ImageIcon(path + "reporte.png");
-        iconoMenu = new ImageIcon(path + "menuPrincipal.png");
-        nombre = new String[10];
-        estatura = new float[10];
+    private static void mostrarReporte() {
+        StringBuilder mensaje = new StringBuilder();
+        for (int i = 0; i < 10; i++) {
+            mensaje.append(nombre[i]).append("      ").append(estatura[i]).append("\n");
+        }
+        JOptionPane.showMessageDialog(null, mensaje, "Reporte", 0, iconoReporte);
     }
-
+    
+   
+    
 }
---
+++ E18ValidarNumero/src/main/java/poo/e18validarnumero/E18ValidarNumero.java
@@ -0,0 +1,20 @@
+package poo.e18validarnumero;
+
+import javax.swing.JOptionPane;
+
+public class E18ValidarNumero {
+
+    public static void main(String[] args) {
+        byte edad;
+        boolean esNumeroValido =false;
+        do {
+            try {
+                edad = Byte.parseByte(JOptionPane.showInputDialog("Edad"));
+                JOptionPane.showMessageDialog(null, "Tu edad es: " + edad);
+                esNumeroValido = true;
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Debes introducir un numero");
+            }
+        } while (!esNumeroValido);
+    }
+}
